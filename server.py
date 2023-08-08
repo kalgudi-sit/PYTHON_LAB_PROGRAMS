7fdd716ff40a5c7f83d0919cbc29eb6121dd62aa
kalgudi-sit <122108281+kalgudi-sit@users.noreply.github.com>
2023-08-09 00:19:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -13,15 +13,15 @@ def server_program():
     conn, address = server_socket.accept() # accept new connection
     print("Connection from: " + str(address))
     while True:
-    # receive data stream. it won't accept data packet greater than
-    1024 bytes
-    data = conn.recv(1024).decode()
-    if not data:
-    # if data is not received break
-    break
-    print("from connected user: " + str(data))
-    data = input(' -> ')
-    conn.send(data.encode()) # send data to the client
+        # receive data stream. it won't accept data packet greater than
+        1024 bytes
+        data = conn.recv(1024).decode()
+        if not data:
+        # if data is not received break
+        break
+        print("from connected user: " + str(data))
+        data = input(' -> ')
+        conn.send(data.encode()) # send data to the client
     conn.close() # close the connection
 
 if __name__ == '__main__':
