cd145960d49d26bac51744ccbc05a36faa0eaf30
kalgudi-sit <122108281+kalgudi-sit@users.noreply.github.com>
2023-09-20 14:33:47 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,8 +6,7 @@ def server_program():
     port = 5000 # initiate port no above 1024
     server_socket = socket.socket() # get instance
     # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port)) # bind host address and port
-    together
+    server_socket.bind((host, port)) # bind host address and port together
     # configure how many client the server can listen simultaneously
     server_socket.listen(2)
     conn, address = server_socket.accept() # accept new connection
